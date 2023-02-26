b5d4460b0650e1cf51e9a8a331597497ec9e59c8
Thrackazoid <105320752+Thrackazoid@users.noreply.github.com>
2023-02-25 21:11:36 -0800

    Initial commit

---
+++ co5c44e798b222ad79bcd55cd/App.js
@@ -0,0 +1,26 @@
+import React from "react"
+import Menu from "./Menu"
+import Favorite from "./Favorite"
+import Toggler from "./Toggler"
+
+function App() {
+    return (
+        <div>
+            <Toggler defaultOnValue={true} render={({on, toggle}) => {
+                return (
+                    <>
+                    <Menu on={on} toggle={toggle}/>
+                    <hr />
+                    </>
+                )
+            }}/>
+            <Toggler defaultOnValue={false} render={({on, toggle}) => {
+                return (
+                    <Favorite on={on} toggle={toggle}/>
+                ) 
+                    }}/>
+        </div>
+    )
+}
+
+export default App
